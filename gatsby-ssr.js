eeae9df9beb62ec3f0695077625c4cc9e084430a
Ariel Villarreal <arirecate_6@hotmail.com>
2020-06-09 20:54:07 -0300

    probando react-spring

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,11 +2,19 @@ import './src/assets/styles/app.css';
 import './src/assets/styles/tailwind.css';
 import './src/assets/styles/googleTranslate.css';
 import './src/assets/styles/vars.css';
-import 'animate.css/animate.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTransition, animated } from 'react-spring';
 import { Header, Footer } from './src/components';
 
 const Body = ({ element }) => {
+  const [show, set] = useState(true);
+  const transitions = useTransition(show, null, {
+    config: { duration: 500, delay: 200 },
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const s = document.createElement('script');
@@ -16,12 +24,18 @@ const Body = ({ element }) => {
       document.body.appendChild(s);
     }
   }, []);
+  console.log(transitions);
   return (
     <div className='min-h-screen'>
       <Header />
-      <div>
-        {element}
-      </div>
+      {transitions.map(({ item, key, props }) => {
+        console.log(props);
+        return (
+          <animated.main key={key} style={props}>
+            {element}
+          </animated.main>
+        );
+      })}
       <Footer />
     </div>
   );
